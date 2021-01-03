19e428d6a7ba478a3c632b6ff50461c91d1ea1f4
atelier-popstar <66830834+atelier-popstar@users.noreply.github.com>
2021-01-03 18:54:00 +0000

    Add files via upload

--- GitAPIInterrogate.py
+++ GitAPIInterrogate.py
@@ -1,16 +1,28 @@
 import sys
 import requests
+import json
 
 def displayDetails():
 	api = 'https://api.github.com/users/'
 	user = input('Please enter the username to display data on: ')
 	fullUrl = api+user
 	response = (requests.get(fullUrl)).json()
-	print(response)
+
+	parsedResponse = json.loads(response)
+
+	
+
+
+
+
+	#print(response)
 	#print(response['followers'])
+	#769b9b7b98a1aacd52d7908d1d489614dc7d74dd
+	#^access token
 
 def main():
 	displayDetails()
 	
 if __name__ == '__main__':
     main()
+
